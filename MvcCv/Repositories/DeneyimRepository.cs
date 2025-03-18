959958362c52ab544c2c65dce4c2820aebd1a6f4
ceydakyaa <88967224+ceydakyaa@users.noreply.github.com>
2025-03-19 01:34:26 +0300

    Add project files.

---
+++ MvcCv/Repositories/DeneyimRepository.cs
@@ -0,0 +1,18 @@
+﻿using MvcCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCv.Repositories
+{
+    public class DeneyimRepository: GenericRepository<TblDeneyimlerim>
+    {
+        DbCvEntities db=new DbCvEntities();
+        public List<TblDeneyimlerim> List()
+        {
+            return db.TblDeneyimlerim.ToList();
+        }
+
+    }
+}
